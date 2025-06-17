e322a4ba6eb57fd20fc2fd1acb8b42d7abd2512c
improdom <30608831+improdom@users.noreply.github.com>
2025-06-17 18:18:42 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,13 +1,5 @@
-// Generate CSV header
-var csv = new System.Text.StringBuilder();
-csv.AppendLine("ColumnName,TableName,DataType,InMemory");
-
-// Generate CSV rows
 foreach (var item in items)
 {
-    var line = $"\"{item.ColumnName}\",\"{item.TableName}\",\"{item.DataType}\",\"{item.InMemory}\"";
+    var line = "\"" + item.ColumnName + "\",\"" + item.TableName + "\",\"" + item.DataType + "\",\"" + item.InMemory + "\"";
     csv.AppendLine(line);
 }
-
-// Output to Tabular Editor's output window
-Output(csv.ToString());
