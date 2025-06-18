f7034e12bd35bcae0f7d21f3c0366207f386a1d5
improdom <30608831+improdom@users.noreply.github.com>
2025-06-18 17:38:50 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,5 +1,10 @@
-foreach (var item in items)
-{
-    var line = "\"" + item.ColumnName + "\",\"" + item.TableName + "\",\"" + item.DataType + "\",\"" + item.InMemory + "\"";
-    csv.AppendLine(line);
-}
+Hi Ankush,
+
+As discussed in our last call, the on-prem team has a set of datasets used for dashboards, with slicing and dicing based on various attributes. In that meeting, you also asked Siva for the list of attributes currently being used for this purpose. Could you please follow up with him to obtain this list?
+
+In the meantime, I’ve configured these same attributes in a query space to model the datasets. This gives us a preliminary view of how the setup would look and helps us assess how much more we can incorporate.
+
+Additionally, I’ve made changes to improve query performance and reorganized the table and sub-model structure to enhance usability.
+
+Best regards,
+Julio
