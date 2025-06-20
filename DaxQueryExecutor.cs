f7219a27a1f1e19b0c199f2683bb9db09e785ba0
improdom <30608831+improdom@users.noreply.github.com>
2025-06-19 21:28:47 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,10 +1,8 @@
-Hi Ankush,
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = null;
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
-As discussed in our last call, the on-prem team has a set of datasets used for dashboards, with slicing and dicing based on various attributes. In that meeting, you also asked Siva for the list of attributes currently being used for this purpose. Could you please follow up with him to obtain this list?
-
-In the meantime, I’ve configured these same attributes in a query space to model the datasets. This gives us a preliminary view of how the setup would look and helps us assess how much more we can incorporate.
-
-Additionally, I’ve made changes to improve query performance and reorganized the table and sub-model structure to enhance usability.
-
-Best regards,
-Julio
+    // Ensure JSON serializer treats DateTime as UTC (still limited)
+});
