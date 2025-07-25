27de74221f337262e41688ca1d4e35482b0c1f90
improdom <30608831+improdom@users.noreply.github.com>
2025-07-25 14:37:16 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,4 +1,1 @@
-<Cancel xmlns="urn:schemas-microsoft-com:xml-analysis">
-  <CancelType>1</CancelType> <!-- 1 = Cancel by Session ID -->
-  <SessionID>your-session-id</SessionID>
-</Cancel>
+CMD ["dotnet", "MyApp.dll", "--urls", "http://+:80"]
