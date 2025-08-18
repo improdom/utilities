c4b05ed0a14fd5ecc83deb7db80fc61e92c033cb
improdom <30608831+improdom@users.noreply.github.com>
2025-08-18 17:41:20 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,5 +1,21 @@
-https://learn.microsoft.com/en-us/fabric/enterprise/powerbi/service-premium-large-models
-
-https://learn.microsoft.com/en-us/fabric/enterprise/powerbi/service-dataset-direct-lake-overview
-
-https://powerbi.microsoft.com/en-us/blog/power-bi-large-models-on-demand-load/
+/* START QUERY BUILDER */
+EVALUATE
+VAR MaxBizDate =
+    CALCULATE (
+        MAX ( pbi_fact_risk_results_aggregated_vw[Business Date] ),
+        ALL ( pbi_fact_risk_results_aggregated_vw[Business Date] )   -- get max available date
+    )
+RETURN
+SUMMARIZECOLUMNS (
+    'CoB Date'[CoB Date],
+    KEEPFILTERS (
+        FILTER (
+            ALL ( pbi_fact_risk_results_aggregated_vw[Business Date] ),
+            pbi_fact_risk_results_aggregated_vw[Business Date] = MaxBizDate
+        )
+    ),
+    "Report Value", [Report Value]
+)
+ORDER BY
+    'CoB Date'[CoB Date] ASC
+/* END QUERY BUILDER */
