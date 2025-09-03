37465c9871b6fd68a0f10d5a7e042e9184208a1c
improdom <30608831+improdom@users.noreply.github.com>
2025-09-03 18:05:44 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,18 +1,7 @@
-EVALUATE
-VAR MaxBizDateTime =
-    CALCULATE (
-        MAX ( pbi_fact_risk_results_aggregated_vw[Business Date] ),
-        ALL ( 'CoB Date' ),                                     -- ignore any date filters
-        ALL ( pbi_fact_risk_results_aggregated_vw[Business Date] )
-    )
-VAR MaxCoBDate = DATEVALUE ( MaxBizDateTime )                   -- align to Date type
-RETURN
-ROW (
-    "CoB Date", MaxCoBDate,
-    "Report Value",
-        CALCULATE (
-            [Report Value],
-            KEEPFILTERS ( TREATAS ( { MaxCoBDate }, 'CoB Date'[CoB Date] ) ),
-            REMOVEFILTERS ( pbi_fact_risk_results_aggregated_vw[Business Date] )
-        )
-)
+Hi Mohan,
+
+I ran the query and did not encounter a timeout error. However, as I suspected, the issue is that the query exceeds the maximum row limit of 1 million rows. To address this, I refactored the DAX to resolve problems related to filters.
+
+Please note that the FILTER function typically generates separate SQL queries where other filters may not be applied, which can significantly increase the row count required to complete the DAX query. Wherever possible, please use KEEPRFILTERS instead of FILTER. Do keep in mind that FILTER and KEEPRFILTERS behave differently depending on the scenario, so it’s important to choose the right one.
+
+Attached is the refactored query for your review.
