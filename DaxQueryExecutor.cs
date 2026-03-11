05c10205f9955ee343e2e5aea859deea45ca005e
improdom <30608831+improdom@users.noreply.github.com>
2026-03-11 13:26:52 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,12 +1,16 @@
-Hi Gopal,
+Hi Venkat,
 
-This is a legacy .NET application running as a service on server nzru0083psq, if I recall correctly. The application was developed a long time ago by a different team that is no longer around.
+Could you please confirm the table name from which we should pick up the update? In your update statement you are referencing neu_dev_at40482_mtrc.cubiq_config.cubiq_metadata, but that table does not appear to exist.
 
-A few years ago I briefly looked into it when there was an issue and I was asked to investigate. At that time the problem turned out to be related to configuration/access.
+I see two other tables:
 
-The service appears as:
+[cubiq].[cubiq_metadata] – this is the table currently referenced by the MRV Builder.
 
-ARiskCeresCubeReconciller – Running – Automatic – ubsprod…
+[config].[cubiq_metadata] – I am not sure how this table is being used.
+
+For our side, the correct table is [cubiq].[cubiq_metadata]. Please confirm that your changes are applied to this table so we can remove the other table if it is not required.
+
+Lokesh, we will also need to include this update in the SQL MI build so it can be deployed to the TEST lane tomorrow.
 
 Thanks,
 Julio Diaz
