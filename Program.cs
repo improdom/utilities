ffbee05a0c1f7e39ecca3edfa9b99b253e084842
improdom <30608831+improdom@users.noreply.github.com>
2026-03-13 18:07:16 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
-Hi Durga / Pradeep,
+Hi Ramesh,
 
-This issue has now been resolved.
+We need to review and determine what information should be displayed in the tooltip. For some MRV definitions, the tooltip text is currently being truncated because it exceeds the allowed length.
 
-As discussed, I reviewed both the logic used to evaluate the trigger schedule and the mechanism responsible for loading the data. The root cause was related to a metadata issue in the on-prem Oracle environment.
-
-While the issue originated upstream, we will introduce additional safeguards on our side to ensure our processes remain protected from similar external conditions. We will also implement an optimization so that data movement is triggered only when updates are detected.
-
-These improvements will be included in the March 27th release, and I will be closely monitoring this change after deployment to ensure it behaves as expected.
+Venkat, could you please open a GitLab issue so we can track and address this?
 
 Thanks,
 Julio Diaz
