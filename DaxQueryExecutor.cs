5f11dcc0da470934b146f9dc7e7d155ea6704ff7
improdom <30608831+improdom@users.noreply.github.com>
2026-06-23 11:10:18 -0400

    Update DaxQueryExecutor.cs

--- DaxQueryExecutor.cs
+++ DaxQueryExecutor.cs
@@ -1,3 +1,42 @@
+IR Zero Delta Bond Future Basis =
+VAR Temp1 =
+    IF(
+        [IR Zero Delta Base - Bond Futures] < 0,
+        -1,
+        1
+    )
+
+VAR Temp2 =
+    IF(
+        [IR Zero Delta Base - Govt./Treasury Bond] < 0,
+        -1,
+        1
+    )
+
+VAR Temp3 =
+    ABS(
+        COALESCE([IR Zero Delta Base - Bond Futures], 0)
+    )
+
+VAR Temp4 =
+    ABS(
+        COALESCE([IR Zero Delta Base - Govt./Treasury Bond], 0)
+    )
+
+RETURN
+    IF(
+        ISBLANK([IR Zero Delta Base - Bond Futures])
+            && ISBLANK([IR Zero Delta Base - Govt./Treasury Bond]),
+        BLANK(),
+        IF(
+            Temp1 = Temp2,
+            0,
+            MIN(Temp3, Temp4)
+        )
+    )
+  
+  
+  
 Hi Ramesh,
 
 As discussed, we removed the list of MRVs approved by MRO/TRO from the CubIQ semantic models. During reconciliation testing, however, we identified several dependency warnings indicating that some of these MRVs are referenced by other MRV definitions.
